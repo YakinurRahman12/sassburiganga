b5db4ac485a6faa5a922cf50904155ef2468941e
YakinurRahman12 <pm.yakinur.rahman@gmail.com>
2024-10-04 02:12:34 +0600

    GSAP addition

---
+++ script.js
@@ -0,0 +1,47 @@
+gsap.to("h1",{
+    x:600,
+    duration:2,
+    delay:0
+})
+gsap.to("img",{
+    x: 505,
+    duration: 3,
+    delay:2,
+    rotate:360,
+    backgroundColor: "blue"
+})
+gsap.to("h2",{
+    y:-250,
+    duration:2,
+    delay:5
+})
+gsap.to("p",{
+    y:-250,
+    duration:3,
+    delay:7
+})
+gsap.to("h1",{
+    y:-83,
+    x:-500,
+    duration:3,
+    delay:10,
+    backgroundColor: "orange"
+})
+gsap.to("li",{
+    y:-250,
+    duration:3,
+    delay:13,
+})
+gsap.to("img",{
+    y:250,
+    duration:2,
+    delay:16,
+    rotate:360
+})
+gsap.to("img",{
+    x:-30,
+    duration:1.5,
+    delay:18,
+    scale:0.7,
+    backgroundColor: "PaleTurquoise"
+})
